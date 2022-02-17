b1c5c69fd629065366c192e6e4c23c895956c85d
Roman Resh <wordsgarden7@gmail.com>
2022-02-18 00:22:21 +0200

    app container width changed

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,7 @@ export default {
 
 <style>
 #app {
+  max-width: 1024px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
